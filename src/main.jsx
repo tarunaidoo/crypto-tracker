de393b2ea9351d0736d20b671a01506b450190f7
tarunaidoo <160468890+tarunaidoo@users.noreply.github.com>
2025-06-26 11:59:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import CoinContextProvider from './context/CoinContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/crypto-tracker">
     <CoinContextProvider>
       <App />
     </CoinContextProvider>
